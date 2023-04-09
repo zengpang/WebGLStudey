88bf073b41757b51501d9aebc0eec145682d56a8
曾胖 <2516629927@qq.com>
2023-04-09 18:13:17 +0800

    更新shader鼠标绘制变色点和绘制点功能

--- CanvasStudey/通过shader实现鼠标绘制变色点功能/canvasPoint.js
+++ CanvasStudey/通过shader实现鼠标绘制变色点功能/canvasPoint.js
@@ -21,7 +21,11 @@ function main() {
     }
     //获取a_Position变量的存储位置
     let a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    //获取u_FragColor变量的存储位置
+    let u_FragColor=gl.getUniformLocation(gl.program,'u_FragColor');
+
     let g_points = [];//鼠标点击位置数组
+    let g_colors=[];//存储点颜色数组
     const click = (ev, gl, canvas, a_Position) => {
         let x = ev.clientX;//鼠点击处的x坐标
         let y = ev.clientY;//鼠标点击处的y坐标
@@ -31,17 +35,34 @@ function main() {
         x = ((x - rect.left) - canvas.height / 2) / (canvas.height / 2);
         y = (canvas.width / 2 - (y - rect.top)) / (canvas.width / 2);
         //将坐标存储到g_points中
-        g_points.push(x); g_points.push(y);
+        g_points.push([x,y]);
+        //将点的颜色存储到g_colors数组中
+        if(x>=0.0&&y>=0.0) //第一象限
+        {
+          g_colors.push([1.0,0.0,0.0,1.0]);//红色
+        }else if(x<0.0&&y<0.0) //第三象限
+        {
+            g_colors.push([0.0,1.0,0.0,1.0]);//红色
+        }
+        else
+        {
+            g_colors.push([1.0,1.0,1.0,1.0]);//红色
+        }
         /**清空<canvas> WebGL中的gl.clear()方法实际上继承自OpenGL
    * ,它基于多基本缓冲区模型，清空绘图区域，实际上是在清空颜色缓冲区(color Buffer)
    * 传递参数gl.COLOR_BUFFER**/
         gl.clear(gl.COLOR_BUFFER_BIT);
         let len = g_points.length;
-        for (let i = 0; i < len; i += 2) {
-            //将点的位置传递到vert即顶点文件变量中a_Position
-            gl.vertexAttrib3f(a_Position, g_points[i], g_points[i + 1], 0.0);
+        for (let i = 0; i < len; i ++) {
+            let xy=g_points[i];
+            let rgba=g_colors[i];
+            console.log(rgba);
+            //将点的位置传递到vert即顶点着色器文件中的变量a_Position
+            gl.vertexAttrib3f(a_Position, xy[0], xy[1],0.0);
+            //将点的颜色传输到frag即片元着色器文件中的变量u_FragColor
+            gl.uniform4f(u_FragColor,rgba[0],rgba[1],rgba[2],rgba[3]);
             //绘制点
-            gl.drawArrays(gl.POINTS, 0, 1);
+            gl.drawArrays(gl.POINTS, 0,1);
         }
     }
     //注册鼠标点击事件响应函数
